d022ec3824912bf354316e3542df4b69aae10b0b
zf07accp <zf07accp@163.com>
2018-05-20 22:07:56 +0800

    chuangjian

---
+++ WHC_DataModelFactory/AppDelegate.m
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.m
+//  WHC_DataModelFactory
+//
+//  Created by 吴海超 on 15/4/30.
+//  Copyright (c) 2015年 吴海超. All rights reserved.
+//
+
+/*
+ *  qq:712641411
+ *  iOS大神qq群:460122071
+ *  gitHub:https://github.com/netyouli
+ *  csdn:http://blog.csdn.net/windwhc/article/category/3117381
+ */
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
---
+++ WHC_DataModelFactory/ViewController.h
@@ -0,0 +1,25 @@
+//
+//  ViewController.h
+//  WHC_DataModelFactory
+//
+//  Created by 吴海超 on 15/4/30.
+//  Copyright (c) 2015年 吴海超. All rights reserved.
+//
+
+
+/*
+ *  qq:712641411
+ *  iOS大神qq群:460122071
+ *  gitHub:https://github.com/netyouli
+ *  csdn:http://blog.csdn.net/windwhc/article/category/3117381
+ */
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
